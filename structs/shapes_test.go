fed09f01a5528ea1151d638a848ead0526cf8e53
Niek den Breeje <n.j.den.breeje@gmail.com>
2018-10-09 17:21:28 +0200

    refact: Learn about table driven tests

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -13,19 +13,18 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	checkArea := func(t *testing.T, shape Shape, want int) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %v, want %v", got, want)
-		}
+	areaTests := []struct {
+		shape Shape
+		want  int
+	}{
+		{Rectangle{12, 6}, 72},
+		{Circle{10}, 314},
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		checkArea(t, Rectangle{12, 6}, 72)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		checkArea(t, Circle{10}, 314)
-	})
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %v, want %v", got, tt.want)
+		}
+	}
 }
