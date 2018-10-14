2aecb7bfde974932c0a966f0d976f5786fb6840a
Niek den Breeje <n.j.den.breeje@gmail.com>
2018-10-14 13:17:59 +0200

    refact: Refactor dictionary & tests

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -6,25 +6,28 @@ func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "This is just a test."}
 
 	t.Run("known word", func(t *testing.T) {
-		got, _ := Search(dictionary, "test")
+		got, _ := dictionary.Search("test")
 		want := "This is just a test."
 
 		assertEquals(t, got, want)
 	})
 
 	t.Run("unknown word", func(t *testing.T) {
-		_, err := Search(dictionary, "bogus")
-		want := "Could not find the word you were looking for."
+		_, got := dictionary.Search("bogus")
 
-		if err == nil {
-			t.Fatal("expected to get an error")
-		}
-
-		assertEquals(t, err.Error(), want)
+		assertError(t, got, ErrNotFound)
 	})
 }
 
 func assertEquals(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got '%s', want '%s'", got, want)
+	}
+}
+
+func assertError(t *testing.T, got, want error) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got '%s', want '%s'", got, want)
 	}
