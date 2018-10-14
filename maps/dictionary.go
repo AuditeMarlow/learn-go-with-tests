0abcdf82f40e174f369190038572af6d927526e7
Niek den Breeje <n.j.den.breeje@gmail.com>
2018-10-14 13:14:17 +0200

    feat: Err check if keyword could not be found

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,7 +1,15 @@
 package maps
 
+import "errors"
+
 type Dictionary map[string]string
 
-func Search(d Dictionary, word string) string {
-	return d[word]
+func Search(d Dictionary, word string) (string, error) {
+	definition, ok := d[word]
+
+	if !ok {
+		return "", errors.New("Could not find the word you were looking for.")
+	}
+
+	return definition, nil
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -5,10 +5,23 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "This is just a test."}
 
-	got := Search(dictionary, "test")
-	want := "This is just a test."
+	t.Run("known word", func(t *testing.T) {
+		got, _ := Search(dictionary, "test")
+		want := "This is just a test."
 
-	assertEquals(t, got, want)
+		assertEquals(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := Search(dictionary, "bogus")
+		want := "Could not find the word you were looking for."
+
+		if err == nil {
+			t.Fatal("expected to get an error")
+		}
+
+		assertEquals(t, err.Error(), want)
+	})
 }
 
 func assertEquals(t *testing.T, got, want string) {
