8f5f4197c5ddb4c237e62737e3e474992d976cbb
Niek den Breeje <n.j.den.breeje@gmail.com>
2018-10-14 16:17:25 +0200

    test: Cover order of writing & sleeping

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
+
+const write = "write"
+const sleep = "sleep"
 
 type SpySleeper struct {
 	Calls int
@@ -13,24 +17,58 @@ func (s *SpySleeper) Sleep() {
 	s.Calls++
 }
 
+type CountdownOperationsSpy struct {
+	Calls []string
+}
+
+func (s *CountdownOperationsSpy) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
 func TestCountdown(t *testing.T) {
-	buffer := &bytes.Buffer{}
-	sleeper := &SpySleeper{}
-	wantedCalls := 3
+	t.Run("prints 3 2 1 Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		sleeper := &SpySleeper{}
+		wantedCalls := 3
 
-	Countdown(buffer, sleeper)
+		Countdown(buffer, sleeper)
 
-	got := buffer.String()
-	want := `3
+		got := buffer.String()
+		want := `3
 2
 1
 Go!`
 
-	if got != want {
-		t.Errorf("got '%s', wanted '%s'", got, want)
-	}
+		if got != want {
+			t.Errorf("got '%s', wanted '%s'", got, want)
+		}
 
-	if sleeper.Calls != wantedCalls {
-		t.Errorf("got %d calls to sleeper, wanted %d", sleeper.Calls, wantedCalls)
-	}
+		if sleeper.Calls != wantedCalls {
+			t.Errorf("got %d calls to sleeper, wanted %d", sleeper.Calls, wantedCalls)
+		}
+	})
+
+	t.Run("sleep after every print", func(t *testing.T) {
+		spySleepPrinter := &CountdownOperationsSpy{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		want := []string{
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
+		}
+	})
 }
